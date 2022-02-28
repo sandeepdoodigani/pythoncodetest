38c4e37636631b308d7ae8ec369045171e737fb9
Gnaneshwar Bandari <31314778+gnaneshwarbandari@users.noreply.github.com>
2022-02-28 15:07:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ try:
     repo.get_contents("/")
     print(repo.get_contents("/"))
 except GithubException as e:
-    print(e.args[1]['message']) 
+    print(e.args[1]['message'])
     # output: This repository is empty.
